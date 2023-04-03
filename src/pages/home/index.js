263de99751aacdcd2d145341643a3524e154e904
Joao Souza <joaoosouza07@gmail.com>
2023-04-03 11:32:32 -0300

    :sparkles: Home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,15 +1,71 @@
-import { View, Text, StyleSheet } from "react-native";
+import { useState } from "react";
+import { View, Text, StyleSheet, SafeAreaView, TextInput, TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+import { Logo } from "../../components/logo";
 
 export function Home() {
+
+    const [inputValue, setInputValue] = useState("");
+
+    function handleSearch() {
+        console.log("Você digitou: " + inputValue);
+    }
+
     return (
-        <View style={styles.container}>
-            <Text>PAGINA HOME</Text>
-        </View>
+        <SafeAreaView  style={styles.container}>
+            <Logo />
+
+            <Text style={styles.title}>Encontre a receita</Text>
+            <Text style={styles.title}>que combina com você</Text>
+
+            <View style={styles.form}>
+                <TextInput 
+                    placeholder="Digite o nome da receita..."
+                    style={styles.input}
+                    value={inputValue}
+                    onChangeText={ (text) => setInputValue(text) }
+                />
+                <TouchableOpacity onPress={handleSearch}>
+                    <Ionicons name="search" size={28} color="#4CBE6C" />
+                </TouchableOpacity>
+            </View>
+
+        </SafeAreaView >
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: 'red'
+        flex: 1,
+        backgroundColor: "#F3F9FF",
+        paddingTop: 60,
+        paddingStart: 20,
+        paddingEnd: 20,
+    },
+    title: {
+        fontSize: 28,
+        fontWeight: 700,
+        color: "#0e0e0e"
+    },
+    form: {
+        backgroundColor: "#FFF",
+        width: "100%",
+        borderRadius: 3,
+        marginTop: 16,
+        marginBottom: 16,
+        borderWidth: 1,
+        borderColor: "#ECECEC",
+        paddingLeft: 10,
+        paddingRight: 10,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between"
+    },
+    input: {
+        width: "90%",
+        maxWidth: "90%",
+        height: 54,
+        fontSize: 16
     }
 })
